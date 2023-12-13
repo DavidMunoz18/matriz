7803c2582193223e564b832f6db257dd887e776b
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2023-12-13 10:53:21 +0100

    Add files via upload

---
+++ matriz/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace matriz
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            int filas = 10;
+            int columnas = 10;
+
+            int[,] matriz = new int[filas, columnas];
+
+            int n = 100;
+            for(int i = 0; i < filas; i++)
+            {
+                for(int j = 0; j < columnas; j++)
+                {
+                    matriz[i, j] = n;
+                    n--;
+                }
+            }
+            Console.WriteLine("Completa de forma automatica numeros del 1 al 100");
+            for (int i = 0;i < filas; i++)
+            {
+                for (int j = 0;j < columnas; j++)
+                {
+                    Console.WriteLine(matriz[i, j]);
+                }
+            }
+            Console.WriteLine("Imprime los numeros en orden ascendente");
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
